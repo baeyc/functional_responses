2f15393ec22db8ba636134e3e717fa1f919aa908
madelattre <maud.delattre@inrae.fr>
2025-07-09 15:56:04 +0200

    fix(several_runs_misspe.py): fix a bug in the loop of experiments per experimental design

--- compute_rmse_misspe.py
+++ compute_rmse_misspe.py
@@ -2,6 +2,10 @@
 # coding: utf-8
 
 # # Effect of a model misspecification
+import os
+import seaborn as sns
+import re
+import collections
 import models
 import jax.numpy as jnp
 import numpy as np
@@ -9,124 +13,132 @@ import matplotlib.pyplot as plt
 import algos
 import pandas as pd
 
-# load results 
+# load results
+
+
 def params_to_array(param):
-    arr = jnp.concatenate([param.pop,param.indiv.mean_latent,param.indiv.cov_latent[jnp.triu_indices(2)],jnp.array([param.var_residual])])
+    arr = jnp.concatenate([param.pop, param.indiv.mean_latent,
+                          param.indiv.cov_latent[jnp.triu_indices(2)], jnp.array([param.var_residual])])
     return arr
+
 
 def theta_to_params(theta):
-    arr = jnp.array([theta[0],theta[1],jnp.log(1+jnp.exp(theta[2]))**2,(jnp.log(1+jnp.exp(theta[3]))**2+theta[4]**2)/2,(jnp.log(1+jnp.exp(theta[2]))*theta[4])/jnp.sqrt(2),jnp.log(1+jnp.exp(theta[5]))])
+    arr = jnp.array([theta[0], theta[1], jnp.log(1+jnp.exp(theta[2]))**2, (jnp.log(1+jnp.exp(theta[3])) **
+                    2+theta[4]**2)/2, (jnp.log(1+jnp.exp(theta[2]))*theta[4])/jnp.sqrt(2), jnp.log(1+jnp.exp(theta[5]))])
     return arr
-    
 
-thetanames = [r"$\mu_{\lambda}$",r'$\mu_h$',r'$\sigma_{\lambda}^2$',r'$\sigma_h^2$',r'$\sigma_{\lambda,h}$',r'$\sigma_0^2$']
 
-true_value = pd.DataFrame({'true_value':[0.7,0.5,0.07,0.05,0.03,0.1**2]})
+thetanames = [r"$\mu_{\lambda}$", r'$\mu_h$', r'$\sigma_{\lambda}^2$',
+              r'$\sigma_h^2$', r'$\sigma_{\lambda,h}$', r'$\sigma_0^2$']
+
+true_value = pd.DataFrame({'true_value': [0.7, 0.5, 0.07, 0.05, 0.03, 0.1**2]})
 true_value['variable'] = thetanames
 
-import collections
 ResEstim = collections.namedtuple("ResEstim",
-        ("theta_wrong","theta_correct","ll_wrong","ll_correct","bic_wrong","bic_correct","y","t","z","zw","zc"))
+                                  ("theta_wrong", "theta_correct", "ll_wrong", "ll_correct", "bic_wrong", "bic_correct", "y", "t", "z", "zw", "zc"))
 
 
 def loadBIC(fn):
-     with open(fn, 'rb') as f:
-          bic = jnp.load(f)  
+    with open(fn, 'rb') as f:
+        bic = jnp.load(f)
 
-     nrep = len(bic)
+    nrep = len(bic)
 
-     # extract information from filename
-     str_split = re.split(r'[_.]', fn)   
-     size = [idx[1:] for idx in str_split if idx[0] == "n" and not idx[1] == "o"]
-     size = int(size[0])
-     dim = [idx[1:] for idx in str_split if idx[0] == "d" and not idx[1] == "i" ]
-     dim = int(dim[0])
-     noise = [idx[-1] for idx in str_split if idx[0] == "m"]
-     noise = int(noise[-1])
-     modtype = "wrong" if 'bicwrong' in str_split else "correct"
-          
-     dbic = pd.DataFrame(bic)
-     dbic.columns = ['BIC']
-     dbic['N'] = size
-     dbic['d'] = dim
-     dbic['noise'] = noise
-     dbic['rep'] = jnp.arange(1,nrep+1)
-     dbic['Model'] = modtype
+    # extract information from filename
+    str_split = re.split(r'[_.]', fn)
+    size = [idx[1:] for idx in str_split if idx[0] == "n" and not idx[1] == "o"]
+    size = int(size[0])
+    dim = [idx[1:] for idx in str_split if idx[0] == "d" and not idx[1] == "i"]
+    dim = int(dim[0])
+    noise = [idx[-1] for idx in str_split if idx[0] == "m"]
+    noise = int(noise[-1])
+    modtype = "wrong" if 'bicwrong' in str_split else "correct"
 
-     return dbic
+    dbic = pd.DataFrame(bic)
+    dbic.columns = ['BIC']
+    dbic['N'] = size
+    dbic['d'] = dim
+    dbic['noise'] = noise
+    dbic['rep'] = jnp.arange(1, nrep+1)
+    dbic['Model'] = modtype
+
+    return dbic
 
 
 def loadtheta(fn):
-     with open(fn, 'rb') as f:
-          theta = jnp.load(f)  
+    with open(fn, 'rb') as f:
+        theta = jnp.load(f)
 
-     (nrep, nparams) = theta.shape
+    (nrep, nparams) = theta.shape
 
-     # extract information from filename
-     str_split = re.split(r'[_.]', fn)   
-     size = [idx[1:] for idx in str_split if idx[0] == "n" and not idx[1] == "o"]
-     size = int(size[0])
-     dim = [idx[1:] for idx in str_split if idx[0] == "d" and not idx[1] == "i" ]
-     dim = int(dim[0])
-     noise = [idx[-1] for idx in str_split if idx[0] == "m"]
-     noise = int(noise[0])
-     
-     theta = theta.reshape((nrep,nparams))
-     dtheta = pd.DataFrame(theta)
-     
-     dtheta['N'] = jnp.repeat(size,nrep)
-     dtheta['d'] = jnp.repeat(dim,nrep)
-     dtheta['noise'] = jnp.repeat(noise,nrep)
-     dtheta['rep'] = jnp.arange(1,nrep+1)
+    # extract information from filename
+    str_split = re.split(r'[_.]', fn)
+    size = [idx[1:] for idx in str_split if idx[0] == "n" and not idx[1] == "o"]
+    size = int(size[0])
+    dim = [idx[1:] for idx in str_split if idx[0] == "d" and not idx[1] == "i"]
+    dim = int(dim[0])
+    noise = [idx[-1] for idx in str_split if idx[0] == "m"]
+    noise = int(noise[0])
 
-     return dtheta
+    theta = theta.reshape((nrep, nparams))
+    dtheta = pd.DataFrame(theta)
+
+    dtheta['N'] = jnp.repeat(size, nrep)
+    dtheta['d'] = jnp.repeat(dim, nrep)
+    dtheta['noise'] = jnp.repeat(noise, nrep)
+    dtheta['rep'] = jnp.arange(1, nrep+1)
+
+    return dtheta
+
 
 # Import BIC
-import os 
-import re
-path = 'results/'
-files = [f for f in os.listdir(path) if re.match(r'modchoice_mecanoise1_d2_biccorrect*', f)]
+path = 'results/'  # change to your results path
+files = [f for f in os.listdir(path) if re.match(
+    r'modchoice_mecanoise1_d2_biccorrect*', f)]
 bic_correct = [loadBIC(os.path.join(path, f)) for f in files]
 bic_correct = pd.concat(bic_correct)
 
-files = [f for f in os.listdir(path) if re.match(r'modchoice_mecanoise1_d2_bicwrong*', f)]
+files = [f for f in os.listdir(path) if re.match(
+    r'modchoice_mecanoise1_d2_bicwrong*', f)]
 bic_wrong = [loadBIC(os.path.join(path, f)) for f in files]
 bic_wrong = pd.concat(bic_wrong)
 
-import seaborn as sns
-bic_all = pd.concat([bic_correct,bic_wrong])
+bic_all = pd.concat([bic_correct, bic_wrong])
 bic_all = bic_all.reset_index()
 
-bp = sns.boxplot(x='N',y='BIC',hue='Model',gap=0.2,fliersize=3,data=bic_all)
+bp = sns.boxplot(x='N', y='BIC', hue='Model',
+                 gap=0.2, fliersize=3, data=bic_all)
 sns.move_legend(bp, "lower center",
-    bbox_to_anchor=(.5, 1), ncol=1, title=None, frameon=False,
-)
+                bbox_to_anchor=(.5, 1), ncol=1, title=None, frameon=False,
+                )
 plt.savefig("bic_missspe_corr_noise.pdf")
 plt.show()
 
 # compute difference in BIC
-bic_correct = bic_correct.rename(columns={'BIC':'BIC correct'})
+bic_correct = bic_correct.rename(columns={'BIC': 'BIC correct'})
 bic_correct = bic_correct.drop(columns=['Model'])
-bic_wrong = bic_wrong.rename(columns={'BIC':'BIC wrong'})
+bic_wrong = bic_wrong.rename(columns={'BIC': 'BIC wrong'})
 bic_wrong = bic_wrong.drop(columns=['Model'])
 bic_diff = bic_correct.merge(bic_wrong)
 bic_diff['diffBIC'] = bic_diff['BIC correct'] - bic_diff['BIC wrong']
 
-bic_diff['diffBICneg'] = bic_diff['diffBIC']<0
+bic_diff['diffBICneg'] = bic_diff['diffBIC'] < 0
 stat_bic = bic_diff.groupby('N').mean()
 stat_bic = stat_bic.reset_index()
-stat_bic = stat_bic[['N','diffBICneg']]
+stat_bic = stat_bic[['N', 'diffBICneg']]
 stat_bic.to_csv('prop_choice_bic_corr_noise.csv')
 
-sns.barplot(data=stat_bic,x='N',y='diffBICneg',width=0.5)
+sns.barplot(data=stat_bic, x='N', y='diffBICneg', width=0.5)
 plt.savefig("prop_choice_bic_corr_noise.pdf")
 
-### Estimation
-files = [f for f in os.listdir(path) if re.match(r'modchoice_mecanoise1_d2_thetacorrect*', f)]
+# Estimation
+files = [f for f in os.listdir(path) if re.match(
+    r'modchoice_mecanoise1_d2_thetacorrect*', f)]
 theta_correct = [loadtheta(os.path.join(path, f)) for f in files]
 theta_correct = pd.concat(theta_correct)
 
-files = [f for f in os.listdir(path) if re.match(r'modchoice_mecanoise1_d2_thetawrong*', f)]
+files = [f for f in os.listdir(path) if re.match(
+    r'modchoice_mecanoise1_d2_thetawrong*', f)]
 theta_wrong = [loadtheta(os.path.join(path, f)) for f in files]
 theta_wrong = pd.concat(theta_wrong)
 
@@ -134,41 +146,47 @@ theta_wrong = pd.concat(theta_wrong)
 theta_correct[thetanames[0]] = theta_correct[0]
 theta_correct[thetanames[1]] = theta_correct[1]
 theta_correct[thetanames[2]] = np.log(1+np.exp(theta_correct[2]))**2
-theta_correct[thetanames[3]] = (np.log(1+np.exp(theta_correct[3]))**2+theta_correct[4]**2)/2
-theta_correct[thetanames[4]] = (np.log(1+np.exp(theta_correct[2]))*theta_correct[4])/np.sqrt(2)
+theta_correct[thetanames[3]] = (
+    np.log(1+np.exp(theta_correct[3]))**2+theta_correct[4]**2)/2
+theta_correct[thetanames[4]] = (
+    np.log(1+np.exp(theta_correct[2]))*theta_correct[4])/np.sqrt(2)
 theta_correct[thetanames[5]] = np.log(1+np.exp(theta_correct[5]))
 
 theta_wrong[thetanames[0]] = theta_wrong[0]
 theta_wrong[thetanames[1]] = theta_wrong[1]
 theta_wrong[thetanames[2]] = np.log(1+np.exp(theta_wrong[2]))**2
-theta_wrong[thetanames[3]] = (np.log(1+np.exp(theta_wrong[3]))**2+theta_wrong[4]**2)/2
-theta_wrong[thetanames[4]] = (np.log(1+np.exp(theta_wrong[2]))*theta_wrong[4])/np.sqrt(2)
+theta_wrong[thetanames[3]] = (
+    np.log(1+np.exp(theta_wrong[3]))**2+theta_wrong[4]**2)/2
+theta_wrong[thetanames[4]] = (
+    np.log(1+np.exp(theta_wrong[2]))*theta_wrong[4])/np.sqrt(2)
 theta_wrong[thetanames[5]] = np.log(1+np.exp(theta_wrong[5]))
 
 
-theta_correct = pd.melt(theta_correct.drop(['rep',0,1,2,3,4,5], axis=1)
-,id_vars=['N','d','noise'])
-theta_wrong = pd.melt(theta_wrong.drop(['rep',0,1,2,3,4,5], axis=1)
-,id_vars=['N','d','noise'])
-
+theta_correct = pd.melt(theta_correct.drop(
+    ['rep', 0, 1, 2, 3, 4, 5], axis=1), id_vars=['N', 'd', 'noise'])
+theta_wrong = pd.melt(theta_wrong.drop(
+    ['rep', 0, 1, 2, 3, 4, 5], axis=1), id_vars=['N', 'd', 'noise'])
 
 
 # Plot the estimates together
 theta_correct["Model"] = "Correct"
 theta_wrong["Model"] = "Wrong"
-dtheta_all = pd.concat([theta_correct,theta_wrong])
+dtheta_all = pd.concat([theta_correct, theta_wrong])
 dtheta_all = dtheta_all.reset_index()
 
 palette = {"Correct": "tab:blue", "Wrong": "tab:orange"}
 
-g = sns.FacetGrid(dtheta_all, col="variable", sharey=False, col_order=thetanames)
-g.map_dataframe(sns.boxplot, x="N", y="value",hue="Model",palette=palette,dodge=True,gap=0.1,fliersize=2,width=0.5)
+g = sns.FacetGrid(dtheta_all, col="variable",
+                  sharey=False, col_order=thetanames)
+g.map_dataframe(sns.boxplot, x="N", y="value", hue="Model",
+                palette=palette, dodge=True, gap=0.1, fliersize=2, width=0.5)
 for ax, pos in zip(g.axes.flat, true_value['true_value']):
     ax.axhline(y=pos, color='r')
 g.add_legend(title='')
-sns.move_legend(g, bbox_to_anchor=(0.5,-0.1), loc='lower center', frameon=False, ncol=2)
+sns.move_legend(g, bbox_to_anchor=(0.5, -0.1),
+                loc='lower center', frameon=False, ncol=2)
 # to remove the prefix with variable name in the facet title
 for item, ax in g.axes_dict.items():
-     ax.grid(False, axis='x')
-     ax.set_title(item) 
+    ax.grid(False, axis='x')
+    ax.set_title(item)
 g.savefig("estim_missspe_corr_noise.pdf")
--- several_runs_misspe.py
+++ several_runs_misspe.py
@@ -1,88 +1,99 @@
+import collections
 import models
 import jax.numpy as jnp
 import numpy as np
 import jax
 from tqdm import tqdm
 import algos
-import math 
+import math
 import pickle
 
 import matplotlib.pyplot as plt
 
 import sys
 
-thetatrue = {"pop" : jnp.array([]), 
-             "indiv" : {"mean_latent" : jnp.array([0.7,0.5]), "cov_latent" : jnp.array([[0.07,0],[0,0.05]])},              
-             "var_residual" : 0.1**2}
+thetatrue = {"pop": jnp.array([]),
+             "indiv": {"mean_latent": jnp.array([0.7, 0.5]), "cov_latent": jnp.array([[0.07, 0], [0, 0.05]])},
+             "var_residual": 0.1**2}
 thetatrue = models.parametrization.params_to_reals1d(thetatrue)
 
-import collections
-ResBIC = collections.namedtuple("ResBIC",("BIC","Loglik","Var_lik"))
+ResBIC = collections.namedtuple("ResBIC", ("BIC", "Loglik", "Var_lik"))
 
 # theta -> array of dim nb_params
 # latent -> array of size nb_iter*nb_params
-def bic(theta,latent,meca_noise,y,d,dim,N=1000):
-    (n,J) = d.shape
-    indiv_lik, var_lik, ll = models.log_likelihood_is(theta,latent,y,d,delta=1,N=N,meca_noise=meca_noise,dim=dim)
+
+
+def bic(theta, latent, meca_noise, y, d, dim, N=1000):
+    (n, J) = d.shape
+    indiv_lik, var_lik, ll = models.log_likelihood_is(
+        theta, latent, y, d, delta=1, N=N, meca_noise=meca_noise, dim=dim)
 #    varll = jnp.var(jnp.nansum(indiv_ll,axis=1))
     bic = -2*ll + 4*jnp.log(n) + jnp.log(n*J)
     return ResBIC(bic, ll, var_lik)
 
 
 # Different sample sizes schemes
-def model_choice_noise(theta,n,J,meca_noise,dim,prng_key,n_preheat,n_iter):
-    key_simu, key_estim, key_ll = jax.random.split(prng_key,num=3)
-    
-    # fix the key in order to have the same density for each fixed value of J    
-    key_d = jax.random.PRNGKey(0)
-    d = jnp.sort(jax.random.choice(key_d,jnp.arange(1,150),shape=(1,J),replace=False))
-    d = jnp.tile(d,(n,1))
-    z, y, my, t, vary = models.simu_data(theta, n=n, d=d, meca_noise=meca_noise, dim=dim,prng_key=key_simu)
+def model_choice_noise(theta, n, J, meca_noise, dim, prng_key, n_preheat, n_iter):
+    key_simu, key_estim, key_ll = jax.random.split(prng_key, num=3)
 
-    thetainit = jax.random.uniform(key=key_simu, shape=(models.parametrization.size,), minval=0.6, maxval=1.4)*thetatrue  
+    # fix the key in order to have the same density for each fixed value of J
+    key_d = jax.random.PRNGKey(0)
+    d = jnp.sort(jax.random.choice(key_d, jnp.arange(
+        1, 150), shape=(1, J), replace=False))
+    d = jnp.tile(d, (n, 1))
+    z, y, my, t, vary = models.simu_data(
+        theta, n=n, d=d, meca_noise=meca_noise, dim=dim, prng_key=key_simu)
+
+    thetainit = jax.random.uniform(key=key_simu, shape=(
+        models.parametrization.size,), minval=0.6, maxval=1.4)*thetatrue
 
     wrong_noise = 1 - meca_noise
 
-    wrong_res = algos.fisher_sgd(y=y,d=d,delta=1,meca_noise=wrong_noise,dim=dim,prng_key=key_estim,pre_heating=n_preheat,Nmax=n_iter,theta0=thetainit,optim_step='AdaGrad',factor=0.85)
-    correct_res = algos.fisher_sgd(y=y,d=d,delta=1,meca_noise=meca_noise,dim=dim,prng_key=key_estim,pre_heating=n_preheat,Nmax=n_iter,theta0=thetainit,optim_step='AdaGrad',factor=0.85)
-    
+    wrong_res = algos.fisher_sgd(y=y, d=d, delta=1, meca_noise=wrong_noise, dim=dim, prng_key=key_estim,
+                                 pre_heating=n_preheat, Nmax=n_iter, theta0=thetainit, optim_step='AdaGrad', factor=0.85)
+    correct_res = algos.fisher_sgd(y=y, d=d, delta=1, meca_noise=meca_noise, dim=dim, prng_key=key_estim,
+                                   pre_heating=n_preheat, Nmax=n_iter, theta0=thetainit, optim_step='AdaGrad', factor=0.85)
+
     N = 1000
-    theta_wrong = jnp.mean(wrong_res.theta[:,-1000:n_iter],axis=0)
-    theta_correct = jnp.mean(correct_res.theta[:,-1000:n_iter],axis=0)
-    latent_wrong = jnp.array(wrong_res.latent)[-1000:,:,:] 
-    latent_correct = jnp.array(correct_res.latent)[-1000:,:,:]
-    bic_wrong, var_ll_wrong, ll_wrong = bic(theta_wrong,latent_wrong,wrong_noise,y,d,dim,N)
-    bic_correct, var_ll_correct, ll_correct = bic(theta_correct,latent_correct,meca_noise,y,d,dim,N)
+    theta_wrong = jnp.mean(wrong_res.theta[:, -1000:n_iter], axis=0)
+    theta_correct = jnp.mean(correct_res.theta[:, -1000:n_iter], axis=0)
+    latent_wrong = jnp.array(wrong_res.latent)[-1000:, :, :]
+    latent_correct = jnp.array(correct_res.latent)[-1000:, :, :]
+    bic_wrong, var_ll_wrong, ll_wrong = bic(
+        theta_wrong, latent_wrong, wrong_noise, y, d, dim, N)
+    bic_correct, var_ll_correct, ll_correct = bic(
+        theta_correct, latent_correct, meca_noise, y, d, dim, N)
 
     return ResEstim(theta_wrong, theta_correct, ll_wrong, ll_correct, bic_wrong, bic_correct, y, t, z, wrong_res, correct_res)
 
 
-import collections
-ResEstim = collections.namedtuple("ResEstim",        ("theta_wrong","theta_correct","ll_wrong","ll_correct","bic_wrong","bic_correct","y","t","z","zw","zc"))
+ResEstim = collections.namedtuple("ResEstim", ("theta_wrong", "theta_correct",
+                                  "ll_wrong", "ll_correct", "bic_wrong", "bic_correct", "y", "t", "z", "zw", "zc"))
 
 keyy = 0
 Nsimus = 1000
-n_vec = jnp.array([10,20,30,40,50,60,70,80,90])
-J_vec = jnp.array([50,25,17,12,10,8,7,6,5])
+n_vec = jnp.array([10, 20, 30, 40, 50, 60, 70, 80, 90])
+J_vec = jnp.array([50, 25, 17, 12, 10, 8, 7, 6, 5])
 meca_noise = 1
 dim = 2
+
 
 def save_object(obj, filename):
     with open(filename, 'wb') as outp:  # Overwrites any existing file.
         pickle.dump(obj, outp, pickle.HIGHEST_PROTOCOL)
 
-theta_wrong = []
-theta_correct = []
-ll_wrong = []
-ll_correct = []
-bic_wrong = []
-bic_correct = []
 
 key_all = jax.random.split(jax.random.PRNGKey(keyy), Nsimus)
 for exp in range(len(n_vec)):
+    theta_wrong = []
+    theta_correct = []
+    ll_wrong = []
+    ll_correct = []
+    bic_wrong = []
+    bic_correct = []
     for i in range(Nsimus):
-	    model_choice_noise(thetatrue,n_vec[exp],J_vec[exp],meca_noise,dim,key_all[i],500,3000)
-
+        res_comp = model_choice_noise(
+            thetatrue, n_vec[exp], J_vec[exp], meca_noise, dim, key_all[i], 500, 3000)
         theta_wrong.append(res_comp.theta_wrong)
         theta_correct.append(res_comp.theta_correct)
         ll_wrong.append(res_comp.ll_wrong)
@@ -90,16 +101,15 @@ for exp in range(len(n_vec)):
         bic_wrong.append(res_comp.bic_wrong)
         bic_correct.append(res_comp.bic_correct)
 
-	with open("modchoice_mecanoise"+str(meca_noise)+"_d"+str(dim)+"_thetawrong_n"+str(n_vec[exp])+"_J"+str(J_vec[exp])+".npy", 'wb') as f:
-	    jnp.save(f,theta_wrong)
-	with open("modchoice_mecanoise"+str(meca_noise)+"_d"+str(dim)+"_thetacorrect_n"+str(n_vec[exp])+"_J"+str(J_vec[exp])+".npy", 'wb') as f:
-	    jnp.save(f,theta_correct)
-	with open("modchoice_mecanoise"+str(meca_noise)+"_d"+str(dim)+"_llwrong_n"+str(n_vec[exp])+"_J"+str(J_vec[exp])+".npy", 'wb') as f:
-	    jnp.save(f,ll_wrong)
-	with open("modchoice_mecanoise"+str(meca_noise)+"_d"+str(dim)+"_llcorrect_n"+str(n_vec[exp])+"_J"+str(J_vec[exp])+".npy", 'wb') as f:
-	    jnp.save(f,ll_correct)
-	with open("modchoice_mecanoise"+str(meca_noise)+"_d"+str(dim)+"_bicwrong_n"+str(n_vec[exp])+"_J"+str(J_vec[exp])+".npy", 'wb') as f:
-	    jnp.save(f,bic_wrong)  
-	with open("modchoice_mecanoise"+str(meca_noise)+"_d"+str(dim)+"_biccorrect_n"+str(n_vec[exp])+"_J"+str(J_vec[exp])+".npy", 'wb') as f:
-	    jnp.save(f,bic_correct) 
-
+    with open("modchoice_mecanoise"+str(meca_noise)+"_d"+str(dim)+"_thetawrong_n"+str(n_vec[exp])+"_J"+str(J_vec[exp])+".npy", 'wb') as f:
+        jnp.save(f, theta_wrong)
+    with open("modchoice_mecanoise"+str(meca_noise)+"_d"+str(dim)+"_thetacorrect_n"+str(n_vec[exp])+"_J"+str(J_vec[exp])+".npy", 'wb') as f:
+        jnp.save(f, theta_correct)
+    with open("modchoice_mecanoise"+str(meca_noise)+"_d"+str(dim)+"_llwrong_n"+str(n_vec[exp])+"_J"+str(J_vec[exp])+".npy", 'wb') as f:
+        jnp.save(f, ll_wrong)
+    with open("modchoice_mecanoise"+str(meca_noise)+"_d"+str(dim)+"_llcorrect_n"+str(n_vec[exp])+"_J"+str(J_vec[exp])+".npy", 'wb') as f:
+        jnp.save(f, ll_correct)
+    with open("modchoice_mecanoise"+str(meca_noise)+"_d"+str(dim)+"_bicwrong_n"+str(n_vec[exp])+"_J"+str(J_vec[exp])+".npy", 'wb') as f:
+        jnp.save(f, bic_wrong)
+    with open("modchoice_mecanoise"+str(meca_noise)+"_d"+str(dim)+"_biccorrect_n"+str(n_vec[exp])+"_J"+str(J_vec[exp])+".npy", 'wb') as f:
+        jnp.save(f, bic_correct)
